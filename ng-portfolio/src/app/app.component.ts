b2aaf44e33f5a6abe478ed268913798ee75d43e7
KagisoMonageng <63680048+KagisoMonageng@users.noreply.github.com>
2023-11-08 06:12:42 +0200

    new section

--- ng-portfolio/src/app/app.component.ts
+++ ng-portfolio/src/app/app.component.ts
@@ -1,4 +1,17 @@
 import { Component } from '@angular/core';
+import { gsap } from "gsap";  
+import { SlowMo } from "gsap/EasePack";
+import { Flip } from "gsap/Flip";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Observer } from "gsap/Observer";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { EaselPlugin } from "gsap/EaselPlugin";
+import { PixiPlugin } from "gsap/PixiPlugin";
+import { TextPlugin } from "gsap/TextPlugin";
+
+
+gsap.registerPlugin(Flip,ScrollTrigger,Observer,ScrollToPlugin,MotionPathPlugin,EaselPlugin,PixiPlugin,TextPlugin,SlowMo);
 
 @Component({
   selector: 'app-root',
@@ -6,5 +19,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-portfolio';
+  title = 'Kagiso | Portfolio';
 }
