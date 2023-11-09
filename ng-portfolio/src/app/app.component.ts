10c10ba8d47d514a5a72a5a2c41b555385bd8e1c
Kagiso Monageng <kagiso.monageng@bastiongroup.co.za>
2023-11-09 10:57:50 +0200

    ready for display

--- ng-portfolio/src/app/app.component.ts
+++ ng-portfolio/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { gsap } from "gsap";  
 import { SlowMo } from "gsap/EasePack";
 import { Flip } from "gsap/Flip";
@@ -18,6 +18,23 @@ gsap.registerPlugin(Flip,ScrollTrigger,Observer,ScrollToPlugin,MotionPathPlugin,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Kagiso | Portfolio';
+export class AppComponent implements OnInit {
+  title = '';
+  ngOnInit(): void {
+   
+    gsap.fromTo(".intro-img",{translateY:0,skewY:0},{translateY:-20,skewY:10,scrollTrigger:{
+      trigger:".intro-img",
+      start:"top 30%"
+      
+    }, onComplete: ()=>{
+      setTimeout(() => {
+        gsap.to(".intro-img",{translateY:0,skewY:0})
+      }, 200);
+    }})
+  }
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    
+  }
+  
 }
